c3e091d1fbebf5c38ef34dd04ee782aa7beff357
Uch19 <145311804+Uch19@users.noreply.github.com>
2025-10-28 14:41:44 -0400

    Add duplicate deletion and user input for linked list
    Implemented a method to delete duplicate nodes from a sorted linked list and added user input functionality to populate the list.

--- homework20_1.java
+++ homework20_1.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 /*
 Given a sorted linked list, delete all nodes that have duplicate
 numbers, leaving only distinct numbers from the original list.In this 
@@ -15,28 +17,62 @@ output: head-->1-->2-->3-->null
 
 Please complete the following program to fullfil the function.
 */
+
 public class homework20_1
 {
    public static void main(String[] args)
    {
-      //add your code here
+      LinkedList llist = new LinkedList();
+      
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Please enter five integers");
+
+      for(int i  = 0; i < 5; i++){
+         System.out.print("Enter Integer " + (i+1) + ": ");
+         int number = sc.nextInt();
+
+         ListNode newNode = new ListNode(number);
+         if(llist.head == null){
+            llist.head = newNode;
+         }else{
+            ListNode current = llist.head;
+            while(current.next != null){
+               current = current.next;
+            }
+            current.next = newNode;
+         }
+         
+      }
+      
+    sc.close();
+    
+    deleteDuplicates(llist);
    
+    System.out.println(llist.toString());
+
    }
-   public static void deleteDuplicates(LinkedList llist)
+     public static void deleteDuplicates(LinkedList llist)
    {
       //add your code here
-   }
+      ListNode current = llist.head;
+      while (current != null && current.next != null){
+         if(current.value == current.next.value){
+            current.next = current.next.next;
+         }else{
+            current = current.next;
+       }  
+     }
 
-}
+  }
 
-class ListNode
+static class ListNode
 {
    int value;
    ListNode next;
    ListNode(int v){value = v;}
 }
 
-class LinkedList
+static class LinkedList
 {
    ListNode head;
    public String toString()
@@ -49,5 +85,6 @@ class LinkedList
          ref = ref.next;
       }
       return "head-->"+nodeData+"null";
-   }
-}
+    }
+  }
+} 
